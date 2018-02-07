3436bf30b18fae5f59ec7973d12103fb5ee5ff5e
mathansurya <35688683+mathansurya@users.noreply.github.com>
2018-02-07 14:27:10 +0530

    Create mathan9.c

---
+++ mathan9.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int l, r; 
+ l = 0;
+ r = arr_size-1; 
+ while (l < r)
+ {
+ if(A[l] + A[r] == sum)
+ return 1; 
+ else if(A[l] + A[r] < s
+ l++;
+ else // A[i] + A[j] > sum
+ }  
+ return 0;
+}
