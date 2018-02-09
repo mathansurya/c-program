d39150f96d8b4f8da46744976452fe22f3d4a50f
mathansurya <35688683+mathansurya@users.noreply.github.com>
2018-02-09 15:57:32 +0530

    Create mathan05.c

---
+++ mathan05.c
@@ -0,0 +1,13 @@
+#include <stdio.h>  
+   
+int main() {  
+    int counter; 
+    printf("Even numbers between 1 to 100\n");
+    counter = 2;
+    while (counter <= 100) {  
+    rintf("%d ", counter);
+    counter = counter + 2;  
+    }  
+   
+    return 0;  
+} 
