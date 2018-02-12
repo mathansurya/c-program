2c92bd430ca6bc9c2cf8ea9f56eed60847d4e477
mathansurya <35688683+mathansurya@users.noreply.github.com>
2018-02-12 13:35:27 +0530

    Update mathan21.c

--- mathan21.c
+++ mathan21.c
@@ -1,5 +1,18 @@
 #include<stdio.h>
+#include<conio.h>
 int main()
 {
+int n,d,f,i,sum=0;
+printf("enter the series");
 scanf("%d",&n);
+printf("enter the difference and first term");
+scanf("%d%d",&d,&f);
+for(i=0;i<n;i++)
+{
+sum=sum+f;
+f=f+d;
+}
+printf("sum is:%d",sum);
+return 0;;
+getch();
 }
