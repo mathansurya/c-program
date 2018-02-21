c28b0304ccb1a4d19e3a489dc2683c666c3e5acc
mathansurya <35688683+mathansurya@users.noreply.github.com>
2018-02-21 14:36:11 +0530

    Create mathan39.c

---
+++ mathan39.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void) 
+{
+	int array[20];
+	int n,i,max=array[0];
+	printf("enter the number of elements:");
+	scanf("%d",&n);
+	printf("\n enter the array elements:");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&array[i]);
+	}
+	for(i=0;i<n;i++)
+	return 0;
+  }
