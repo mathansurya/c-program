20ee9b1f5916ed0771bd0fd0cfa6479298290da7
mathansurya <35688683+mathansurya@users.noreply.github.com>
2018-03-16 15:54:31 +0530

    Create mathan100.c

---
+++ mathan100.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h> 
+int main() 
+{
+int n,remainder,product=1;
+scanf("%d",&n);
+while(n!=0)
+{
+remainder=n%10;
+product=product*remainder;
+n=n/10;
+}
+printf("\n%d",product);
+return 0;
+}
