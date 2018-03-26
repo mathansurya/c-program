487fa47ed86521d224f8fb78cde70be0af6d8cd6
mathansurya <35688683+mathansurya@users.noreply.github.com>
2018-03-26 15:47:53 +0530

    Create mathan101.c

---
+++ mathan101.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char str[50];
+	int i,n;
+	scanf("%s %d",str,&n);
+	int len;
+	len=strlen(str);
+	for(i=n;i<=len;i++)
+	{
+		printf("%c",str[i]);
+	}
+
+	return 0;
+}
