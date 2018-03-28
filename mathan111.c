7e86c648aec672d7247cc3e0c19ce9f6772101b3
mathansurya <35688683+mathansurya@users.noreply.github.com>
2018-03-28 15:50:27 +0530

    Create mathan111.c

---
+++ mathan111.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int main() 
+{
+
+	int num,count=0;
+
+	printf("enter the number");
+
+	scanf("%d",&num);
+
+	while(num!=0)
+
+	{
+
+		num=num/10;
+
+		count++;
+
+	}
+
+	printf("\n%d",count);
+
+	return 0;
+
+}
